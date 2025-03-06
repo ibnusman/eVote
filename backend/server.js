5264fa8c4dba499c903e538c5817d510186114b8
ibnusman <abusaleh3035@gmail.com>
2025-03-06 10:39:07 +0100

    Hash password

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,33 @@
 import express from 'express';
 import 'dotenv/config'
 import bodyParser from 'body-parser'
-
+import passport from 'passport';
+// import LocalStrategy from 'passport-local'
+import bcrypt from "bcryptjs";
 
 const port = process.env.PORT;
 const app = express()
-
+//Set salt round
+const salt = bcrypt.genSaltSync(10);
+//Setting middleware
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//homepage
 app.get('/', function (req, res) {
   res.send('Voting System')
 
 })
 
-
+//registration page
 app.get('/register', function (req, res) {
 
   const email = req.body.email;
   const username = req.body.username;
   const password = req.body.password;
-
-
-  console.log(`your username is ${username} and password ${password}`)
+const hash = bcrypt.hashSync(password, salt);
+  console.log(`your username is ${username} and password ${hash}`)
   res.send('Registration page')
 
 })
