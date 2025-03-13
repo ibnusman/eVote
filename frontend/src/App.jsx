c024a5e6bafd101d6eb3ab10a3143aefca8bdb72
ibnusman <abusaleh3035@gmail.com>
2025-03-13 11:37:26 +0100

    OTP page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Login } from './components/Login'
 import { Signup } from './components/Signup'
+import { TwoFA } from './components/TwoFA.JSX'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       
        
       {/* <Signup/> */}
-      <Login/>
+<TwoFA/>
+      {/* <Login/> */}
     </>
   )
 }
