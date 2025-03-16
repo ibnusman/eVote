ea4f632160bc4300960079c4443465a45e73be27
ibnusman <abusaleh3035@gmail.com>
2025-03-16 13:53:05 +0100

    Route signup

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
+
 
 export function SignupForm() {
+    const navigate = useNavigate();
     const [formData, setFormData] = useState({
         fname: "",
         sname: "",
@@ -10,6 +13,7 @@ export function SignupForm() {
         username: "", 
         pass: ""
     });
+    const [message,setMessage] = useState("")
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -18,15 +22,25 @@ export function SignupForm() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        setMessage(null); 
         try {
             const response = await axios.post("http://localhost:3000/api/auth/register", formData, {
                 headers: { 'Content-Type': 'application/json' }
             });
             console.log(response.data);
+            setMessage(`${response.data.message} Loading login page...`);
+            setTimeout(() => {
+                navigate('/login')
+            }, 3000);
             
         } catch (error) {
             console.error("Error adding user", error.data);
+            if(error.response.data){
+
+            setMessage(error.response.data.message)
+            }else{
+                setMessage("Server Error")
+            }
         }
 
         
@@ -100,6 +114,7 @@ export function SignupForm() {
                         className="mt-1 p-3 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                 </div>
+             {message && <div className="text-red-500">{message}</div>}
 
                 <button 
                     type="submit"
