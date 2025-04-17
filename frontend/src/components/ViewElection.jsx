c3e4800051cc1c7f3a30dcb2d053de1250873826
ibnusman <abusaleh3035@gmail.com>
2025-04-17 17:08:34 +0100

    feat search bar

--- frontend/src/components/ViewElection.jsx
+++ frontend/src/components/ViewElection.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
-import { Edit, Trash2 } from "lucide-react";
+import { Edit, Search, Trash2 } from "lucide-react";
 
 export const ViewElection = () => {
   const [elections, setElections] = useState([]);
@@ -10,6 +10,12 @@ export const ViewElection = () => {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
   const [sort, setSort] = useState("");
+  const [search,setSearch] = useState("");
+    const [list, setList] = useState([
+    "Go to the store",
+    "Wash the dishes",
+    "Learn some code",
+  ]);
 
   useEffect(() => {
     const fetchElection = async () => {
@@ -63,11 +69,36 @@ export const ViewElection = () => {
     console.log(allElections);
   };
 
+  const searchElection = (e) => {
+   const sValue = e.target.value;
+     setSearch(sValue);
+
+    if(sValue == ""){
+      setElections(allElections);
+    }
+    const seElec = allElections.filter((elec)=> elec.position.toString().toLowerCase().includes(sValue.toLowerCase()))
+     setElections(seElec);
+     
+
+  }
+
   return (
     <div>
       <h2 className="text-xl font-semibold text-gray-700 mb-4">Election List</h2>
 
       {/* Sorting Dropdown */}
+   <label htmlFor="search" className="block text-sm font-medium text-gray-600 mb-1">Search by Position</label>
+<input
+  type="text"
+  name="search"
+  id="search"
+  value={search}
+  onChange={searchElection}
+  placeholder="e.g. President"
+  className="w-full sm:w-64 px-4 py-2 mb-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+/>
+
+
       <div className="mb-6">
         <label htmlFor="positions" className="block text-sm font-medium text-gray-600 mb-1">
           Filter by Positions
