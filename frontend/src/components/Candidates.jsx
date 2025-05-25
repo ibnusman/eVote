61811be1b6c827d02c9408359669be93ce1ffe1b
ibnusman <abusaleh3035@gmail.com>
2025-05-25 23:17:41 +0100

    votes

--- frontend/src/components/Candidates.jsx
+++ frontend/src/components/Candidates.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { FaVoteYea } from "react-icons/fa"; // Icon library - install it
+import { FaVoteYea } from "react-icons/fa"; // Icon library install it
 import { useParams } from 'react-router-dom';
 
 export function Candidates() {
